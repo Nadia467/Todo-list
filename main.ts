ba9921cc09d08727e957c4e845c84b96cec29422
Nadia <161623420+Nadia467@users.noreply.github.com>
2024-07-16 21:37:50 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let todos :string[]= [];
+let condition = true;
+while (condition) {
+  let addTask = await inquirer.prompt([
+    {
+      name: "todo",
+      message: "what you want to do in your todos?",
+      type: "input",
+    },
+    {
+      name: "addMore",
+      message: "do you want to add more?",
+      type: "confirm",
+      default: true,
+    },
+  ]);
+
+  todos.push(addTask.todo);
+  condition = addTask.addMore;
+  console.log(todos);
+}
+
